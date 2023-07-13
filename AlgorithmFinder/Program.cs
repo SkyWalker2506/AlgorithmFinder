8736c5b0ce67d8e044512bc7b0d71dcb4d88b210
SkyWalker2506 <42008533+SkyWalker2506@users.noreply.github.com>
2023-07-13 14:36:27 +0300

    Update

--- AlgorithmFinder/Program.cs
+++ AlgorithmFinder/Program.cs
@@ -17,81 +17,157 @@ void FindAlgorithm()
 {
     string algorithm = "";
 
-    foreach (var combination in GetPairs(numbers))
+    NumbersWithEarlierOperations numbersWithEarlierOperations = new NumbersWithEarlierOperations
     {
-        foreach (float value in combination)
-        {
-            Console.Write(value+", ");
-        }
-        Console.WriteLine("");
-        Console.WriteLine("Next combination");
+        Numbers = numbers,
+        EarlierOperations = new List<NumbersWithOperation>()
+    };
 
+    List<NumbersWithEarlierOperations> numbersWithEarlierOperationsList =
+        new List<NumbersWithEarlierOperations> { numbersWithEarlierOperations };
+    
+    while (numbersWithEarlierOperationsList.Count > 0)
+    {
+        List<NumbersWithEarlierOperations> newList = numbersWithEarlierOperationsList;
+        foreach (NumbersWithEarlierOperations nweo in numbersWithEarlierOperationsList)
+        {
+            newList.AddRange(GetListNumbersWithEarlierOperation(nweo));   
+        }
+
+        numbersWithEarlierOperationsList = newList;
+        foreach (NumbersWithEarlierOperations nweo in numbersWithEarlierOperationsList)
+        {
+            if (nweo.Numbers.Length == 1)
+            {
+                
+            }
+        }
     }
 
+    if (numbersWithEarlierOperationsList.Count == 0)
+    {
+        Console.Write($"There is no solution fo find {result} with given numbers");
+    }  
+    
 }
 
 
-List<NumbersWithEarlierOperation> GetListNumbersWithEarlierOperation(NumbersWithEarlierOperation numbersWithEarlierOperation)
+List<NumbersWithEarlierOperations> GetListNumbersWithEarlierOperation(NumbersWithEarlierOperations numbersWithEarlierOperation)
 {
-    List<NumbersWithEarlierOperation> numbersWithEarlierOperations = new List<NumbersWithEarlierOperation>();
+    List<NumbersWithEarlierOperations> numbersWithEarlierOperations = new List<NumbersWithEarlierOperations>();
 
-    
+    List<float[]> pairs = GetPairs(numbersWithEarlierOperation.Numbers);
+
+    foreach (float[] pair in pairs)
+    {
+        
+    }
     
     return numbersWithEarlierOperations;
 }
 
-float Calculate(float value1, float value2, Operations operation)
+float Calculate(NumbersWithOperation numbersWithOperation)
 {
-    switch (operation)
+    switch (numbersWithOperation.Operation)
     {
-        case Operations.Sum:
-            return value1+value2;
-        case Operations.Subtraction:
-            return value1-value2;
-        case Operations.Multiplication:
-            return value1*value2;
-        case Operations.Division:
-            return value2==0?float.MaxValue:value1/value2;
+        case Operation.Sum:
+            return numbersWithOperation.Number1 + numbersWithOperation.Number1;
+        case Operation.Subtraction:
+            return numbersWithOperation.Number1 -numbersWithOperation.Number1 ;
+        case Operation.Multiplication:
+            return numbersWithOperation.Number1 * numbersWithOperation.Number1 ;
+        case Operation.Division:
+            return numbersWithOperation.Number1 == 0?float.MaxValue:numbersWithOperation.Number1 / numbersWithOperation.Number1 ;
     }
 
     return 0;
 }
 
-string OperationToString(Operations operation)
+string GetCalculationStringNumbersWithOperation(NumbersWithOperation numbersWithOperation)
+{
+    return GetCalculationString(numbersWithOperation.Number1.ToString(), numbersWithOperation.Number2.ToString(),
+        numbersWithOperation.Operation);
+}
+
+string GetCalculationString(string value1, string value2, Operation operation)
 {
     switch (operation)
     {
-        case Operations.Sum:
+        case Operation.Sum:
+            return $"({value1} + {value2})";
+        case Operation.Subtraction:
+            return $"({value1} - {value2})";
+        case Operation.Multiplication:
+            return $"{value1} * {value2}";
+        case Operation.Division:
+            return $"{value1} / {value2}";
+    }
+
+    return "";
+}
+
+
+
+
+string GetEarlierOperationsString(List<NumbersWithOperation> earlierOperations)
+{
+
+    if (earlierOperations.Count == 0)
+    {
+        return "";
+    }
+
+    string result = GetCalculationStringNumbersWithOperation(earlierOperations[0]);
+
+    for (int i = 1; i < earlierOperations.Count; i++)
+    {
+        result = GetCalculationString(result, GetCalculationStringNumbersWithOperation(earlierOperations[i]),
+            earlierOperations[i].Operation);
+    }
+    
+    return result;
+}
+
+
+string OperationToString(Operation operation)
+{
+    switch (operation)
+    {
+        case Operation.Sum:
             return "+";
-        case Operations.Subtraction:
+        case Operation.Subtraction:
             return "-";
-        case Operations.Multiplication:
+        case Operation.Multiplication:
             return "*";
-        case Operations.Division:
+        case Operation.Division:
             return "/";
     }
 
     return "";
 }
 
-IEnumerable<float[]> GetPairs(float[] array)
+List<float[]> GetPairs(float[] array)
 {
+    List<float[]> pairs = new List<float[]>();
+    
     for (int i = 0; i < array.Length; i++)
     {
         for (int j = i + 1; j < array.Length; j++)
         {
             if (array[i] != array[j])
             {
-                yield return new[] { array[i], array[j] };
-                yield return new[] { array[j], array[i] };
+                pairs.Add(new[] { array[i], array[j] });
+                pairs.Add(new[] { array[j], array[i] });
             }
         }
     }
+
+    return pairs;
 }
 
 
 
-enum Operations
+enum Operation
 {
     Sum,
     Subtraction,
@@ -100,8 +176,18 @@ enum Operations
 }
 
 
-struct NumbersWithEarlierOperation
+struct NumbersWithEarlierOperations
 {
-    public float[] Numbers; 
-    public string Operations;
+    public float[] Numbers;
+    public List<NumbersWithOperation> EarlierOperations;
+    
 }
+
+struct NumbersWithOperation
+{
+    public float Number1; 
+    public float Number2; 
+    public Operation Operation;
+}
+
+
